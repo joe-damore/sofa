704b823fc14ab0762c609d4e9770e23f21c3c6e3
Joe D'Amore <joe@joedamore.me>
2021-12-09 13:04:24 -0500

    Add Electron hello world

--- core/sofa-core/src/main/index.ts
+++ core/sofa-core/src/main/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello, world!');
+import { app, BrowserWindow, ipcMain } from 'electron';
+import { Store } from '@src/core/data/store';
+
+import path from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      devTools: true,
+      preload: path.resolve('./dist/renderers/app/renderer.preload.js'),
+    },
+  });
+
+  win.webContents.openDevTools();
+
+  win.loadFile('./dist/renderers/app/index.html');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
